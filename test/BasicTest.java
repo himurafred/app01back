e8cf5ba7bc34d9010658a4dd850232220a1b2b63
slouis <louis.sebastien@gmail.com>
2012-01-07 16:51:01 +0100

    first test

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -5,9 +5,22 @@ import models.*;
 
 public class BasicTest extends UnitTest {
 
+    @Before
+    public void setup() {
+        Fixtures.deleteAll();
+    }
+
     @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
+    public void createAndRetrieveListElement() {
+        // Create a new user and save it
+        ListElement save = new ListElement("Ma super liste").save();
+
+        // Retrieve the user with bob username
+        ListElement read = ListElement.find("uuid", save.uuid).first();
+
+        // Test 
+        assertNotNull(read);
+        assertEquals(save, read);
     }
 
 }
